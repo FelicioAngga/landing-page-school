3c93ddbd594fdba07e05e3090328d732dab61ee4
felicio.angga <211110176@students.mikroskil.ac.id>
2025-06-23 09:39:23 +0700

    fix: fix news when search

--- src/features/news/index.tsx
+++ src/features/news/index.tsx
@@ -2,7 +2,7 @@ import { IoIosSearch } from "react-icons/io";
 import Footer from "./components/Footer";
 import NewsItem from "./components/NewsItem";
 import ReactPaginate from "react-paginate";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getNews, NewsResponseType } from "./services/news-service";
 
@@ -29,47 +29,53 @@ export default function () {
     setPaginateData((prev) => ({ ...prev, page: 1, search }));
   }
 
-  return (
-    <form onSubmit={handleSubmit} className="min-h-[93vh] flex flex-col justify-between">
-      <div className="mb-3 py-5 px-4 md:px-[60px] 2xl:py-7 2xl:px-28">
-        <p className="w-fit mx-auto text-2xl md:text-3xl font-bold">
-          Berita Terbaru
-        </p>
-        <div className="relative mx-auto w-fit mt-6">
-          <IoIosSearch
-            className="absolute inset-y-0 my-auto left-1.5"
-            size={24}
-          />
-          <input
-            type="text"
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder="Cari berita (Enter untuk mencari)"
-            className="md:w-96 py-2 pr-3 pl-8 rounded border border-[#828282]"
-          />
-        </div>
-        
-        <div className="flex flex-wrap">
-          {newsData?.data.map((item, idx) => <NewsItem key={idx} {...item} />)}
-        </div>
+  useEffect(() => {
+    if (search === "") setPaginateData((prev) => ({ ...prev, page: 1, search: "" }));
+  }, [search])
 
-        <ReactPaginate
-          breakLabel="..."
-          nextLabel="Next"
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={5}
-          forcePage={paginateData.page - 1}
-          pageCount={pageCount}
-          previousLabel="Previous"
-          renderOnZeroPageCount={null}
-          className="flex flex-wrap mt-6 justify-center md:justify-end"
-          previousClassName="rounded-l text-gray-600 font-medium cursor-pointer border border-gray-300 px-2 py-1.5"
-          nextClassName="rounded-r text-gray-600 font-medium cursor-pointer border border-gray-300 px-4 py-1.5"
-          pageClassName="border border-gray-300 px-3 py-1.5"
-          activeClassName="bg-blue-500 text-white"
-          breakClassName="border border-gray-300 px-3 py-1.5"
-        />
-      </div>
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="min-h-[60vh] flex flex-col justify-between">
+        <div className="mb-3 py-5 px-4 md:px-[60px] 2xl:py-7 2xl:px-28">
+          <p className="w-fit mx-auto text-2xl md:text-3xl font-bold">
+            Berita Terbaru
+          </p>
+          <div className="relative mx-auto w-fit mt-6">
+            <IoIosSearch
+              className="absolute inset-y-0 my-auto left-1.5"
+              size={24}
+            />
+            <input
+              type="text"
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Cari berita (Enter untuk mencari)"
+              className="md:w-96 py-2 pr-3 pl-8 rounded border border-[#828282]"
+            />
+          </div>
+          
+          <div className="flex flex-wrap">
+            {newsData?.data.map((item, idx) => <NewsItem key={idx} {...item} />)}
+          </div>
+
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="Next"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            forcePage={paginateData.page - 1}
+            pageCount={pageCount}
+            previousLabel="Previous"
+            renderOnZeroPageCount={null}
+            className="flex flex-wrap mt-6 justify-center md:justify-end"
+            previousClassName="rounded-l text-gray-600 font-medium cursor-pointer border border-gray-300 px-2 py-1.5"
+            nextClassName="rounded-r text-gray-600 font-medium cursor-pointer border border-gray-300 px-4 py-1.5"
+            pageClassName="border border-gray-300 px-3 py-1.5"
+            activeClassName="bg-blue-500 text-white"
+            breakClassName="border border-gray-300 px-3 py-1.5"
+          />
+        </div>
+      </form>
       <Footer />
-    </form>
+    </div>
   );
 }
